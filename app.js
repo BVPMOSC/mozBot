83ab2c0d596f1ab57c7f61e37355acce2b1acea0
Piyush Agrawal <alltimepresent@gmail.com>
2017-08-21 20:22:37 +0530

    Refactoring and adding node package.json

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ if (!process.env.GITHUB_TOKEN) {
   console.error('GITHUB_TOKEN=insert_token_here npm start');
   console.error('5) Run the following command in another tab:');
   console.error('curl -X POST -d @__tests__/data/23.webhook http://localhost:5000/');
-  console.error('6) Check if it has commented here: https://github.com/fbsamples/bot-testing/pull/23');
+  console.error('6) Check if it has commented here: https://github.com/bvpmosc/bot-testing/pull/23');
   process.exit(1);
 }
 
@@ -52,36 +52,6 @@ github.authenticate({
 var app = express();
 //core function start
 // read github api for nodejs
-function buildMentionSentence(reviewers) {
-  var atReviewers = reviewers.map(function(owner) { return '@' + owner; });
-
-  if (reviewers.length === 1) {
-    return atReviewers[0];
-  }
-
-  return (
-    atReviewers.slice(0, atReviewers.length - 1).join(', ') +
-    ' and ' + atReviewers[atReviewers.length - 1]
-  );
-}
-
-function defaultMessageGenerator(reviewers, pullRequester) {
-  return util.format(
-    '%s, thanks for your PR! ' +
-    'By analyzing the history of the files in this pull request' +
-    ', we identified %s to be%s potential reviewer%s.',
-    pullRequester,
-    buildMentionSentence(reviewers),
-    reviewers.length > 1 ? '' : ' a',
-    reviewers.length > 1 ? 's' : ''
-  );
-}
-
-function configMessageGenerator(message, reviewers, pullRequester) {
-  var withReviewers = message.replace(/@reviewers/g, buildMentionSentence(reviewers));
-  return withReviewers.replace(/@pullRequester/g, pullRequester);
-}
-
 function getRepoConfig(request) {
   return new Promise(function(resolve, reject) {
     github.repos.getContent(request, function(err, result) {
@@ -103,42 +73,42 @@ function getRepoConfig(request) {
 }
 
  async function work(body) {
-  var data = {};
-  try {
-    data = JSON.parse(body.toString());
-    console.log(data.pull_request.html_url);
-  } catch (e) {
-    console.error(e);
-    function assignRequestCreater(data, reject){
-
-      function getRequest(){
-        github.pullRequests.get({
-        owner: data.repository.owner.login,
-        repo: data.repository.name,
-        number: data.pull_request.number
-      }, function(err,result) {
-        if (err) {
-          reject(err);
-        }
-        resolve(result);
-      });
-    }
-
-    github.issues.addAssigneesToRequest({
-      owner: data.repository.owner.login,
-      repo: data.repository.name,
-      number: data.pull_request.number,
-      assignee: user
-    }, function(err) {
-      if (err) {
-        if (typeof reject === 'function') {
-          return reject(err);
-        }
+      var data = {};
+      try {
+        data = JSON.parse(body.toString());
+        console.log(data.pull_request.html_url);
+      } catch (e) {
+        console.error(e);
       }
-    });
-  }
-  }
-  };
+
+    function assignRequestCreater(data, reject){
+      
+            github.pullRequests.get({
+                owner: data.repository.owner.login,
+                repo: data.repository.name,
+                number: data.pull_request.number
+             }, function(err,result) {
+                if (err) {
+                    reject(err);
+                }
+
+                github.issues.addAssigneesToIssue({
+                  owner: data.repository.owner.login,
+                  repo: data.repository.name,
+                  number: data.pull_request.number,
+                  assignee: result.user.login
+                }, function(err, result) {
+                    if (err) {
+                        if (typeof reject === 'function') {
+                          return reject(err);
+                        }
+                    }
+                    resolve(result)
+                });
+            });
+        
+    }
+};
 
 app.post('/', function(req, res) {
   req.pipe(bl(function(err, body) {
@@ -154,8 +124,8 @@ app.post('/', function(req, res) {
 
 app.get('/', function(req, res) {
   res.send(
-    'GitHub Mention Bot Active. ' +
-    'Go to https://github.com/facebook/mention-bot for more information.'
+    '' +
+    'Go to https://github.com/bvpmosc/mozBot for more information.'
   );
 });
 
--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 
 var exports = module.exports = {};
 
-var packageConfig = require('./package.json').config;
+// var packageConfig = require('./package.json').config;
 var configJson = require('./config.json');
 
-var mergedConfig = Object.assign(packageConfig, configJson);
+// var mergedConfig = Object.assign(packageConfig, configJson);
 
 exports.github = {
-  host: process.env.GHE_HOST || mergedConfig.gheHost || 'github.com',
-  apiHost: process.env.GHE_API_HOST || mergedConfig.gheHost || 'api.github.com',
-  protocol: process.env.GHE_PROTOCOL || mergedConfig.gheProtocol || 'https',
-  pathPrefix: process.env.GHE_PATH_PREFIX || mergedConfig.ghePathPrefix,
-  port: process.env.GHE_PORT || mergedConfig.ghePort || 443
+  host: process.env.GHE_HOST || configJson.gheHost || 'github.com',
+  apiHost: process.env.GHE_API_HOST || configJson.gheHost || 'api.github.com',
+  protocol: process.env.GHE_PROTOCOL || configJson.gheProtocol || 'https',
+  pathPrefix: process.env.GHE_PATH_PREFIX || configJson.ghePathPrefix,
+  port: process.env.GHE_PORT || configJson.ghePort || 443
 };
